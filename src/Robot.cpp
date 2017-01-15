2b40484477088fa0708dcd84d91621ddf37ffe57
Kevin Allen <kma1660@gmail.com>
2017-01-14 20:30:46 -0500

    update with latst rosfrc stuff

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,10 +2,23 @@
 // Ros stuff
 #include <rosfrc/RosRobot.h>
 #include <std_msgs/String.h>
+
+// FRC Stuff
 #include <RobotDrive.h>
 #include <Talon.h>
+#include <Encoder.h>
+#include <BuiltInAccelerometer.h>
+
 #include <memory>
 
+/*
+constexpr const char* OnboardResourceVISA = "ASRL1::INSTR";
+constexpr const char* MxpResourceVISA = "ASRL2::INSTR";
+
+constexpr const char* OnboardResourceOS = "/dev/ttyS0";
+constexpr const char* MxpResourceOS = "/dev/ttyS1";
+
+ */
 class Robot: public RosRobot {
 public:
 	ros::Publisher chatter;
@@ -14,6 +27,9 @@ public:
 	std::shared_ptr<frc::Talon> BR;
 	std::shared_ptr<frc::Talon> FL;
 	std::shared_ptr<frc::Talon> FR;
+	std::shared_ptr<BuiltInAccelerometer> accel;
+	std::shared_ptr<frc::Encoder> encoder_left;
+	std::shared_ptr<frc::Encoder> encoder_right;
 	frc::RobotDrive drive;
 	std_msgs::String str_msg;
 	Robot():
@@ -24,6 +40,9 @@ public:
 			BR(new Talon(2)),
 			FL(new Talon(0)),
 			FR(new Talon(1)),
+			accel(new BuiltInAccelerometer()),
+			encoder_left(new Encoder(0, 1)),
+			encoder_right(new Encoder(2, 3)),
 			drive(*FL, *BL, *FR, *BR)
 	{
 		FR->SetInverted(true);
@@ -37,6 +56,9 @@ public:
 		AddSpeedController("/speed_controllers/FR", FR);
 		AddSpeedController("/speed_controllers/BL", BL);
 		AddSpeedController("/speed_controllers/BR", BR);
+		AddEncoder("/encoder_left", encoder_left);
+		AddEncoder("/encoder_right", encoder_right);
+		AddAccelerometer("/accel", accel);
 		getRosNodeHandle().advertise(chatter);
 	}
 	void RobotPeriodic () override
